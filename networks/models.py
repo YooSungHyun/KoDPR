8906c3a5cac9186f8a4134fa75c03edc6b0df0b4
YooSungHyun <shyu0522@hanmail.net>
2024-02-13 01:19:39 +0900

    Init: model init

--- networks/models.py
+++ networks/models.py
@@ -1,17 +1,46 @@
-import torch.nn as nn
+import transformers
+from transformers import DebertaV2Model
+from transformers.models.deberta_v2.modeling_deberta_v2 import ContextPooler
+
+import torch
+import logging
+import os
+from copy import deepcopy
 
 
-class Net(nn.Module):
+class KobertBiEncoder(torch.nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.lstm1 = nn.LSTM(7, 32, num_layers=1, batch_first=True)
-        self.dropout = nn.Dropout(0.2)
-        self.lstm2 = nn.LSTM(32, 16, num_layers=1, batch_first=True)
-        self.fc = nn.Linear(16, 1, bias=True)
+        super(KobertBiEncoder, self).__init__()
+        self.passage_encoder = DebertaV2Model.from_pretrained("team-lucid/deberta-v3-base-korean")
+        self.query_encoder = DebertaV2Model.from_pretrained("team-lucid/deberta-v3-base-korean")
 
-    def forward(self, inputs):
-        hidden, _ = self.lstm1(inputs)
-        hidden, _ = self.lstm2(hidden)
-        hidden = hidden[:, -1, :]
-        logit = self.fc(hidden)
-        return logit
+    def forward(
+        self, x: torch.LongTensor, attn_mask: torch.LongTensor, type: str = "passage"
+    ) -> torch.FloatTensor:
+        """passage 또는 query를 bert로 encoding합니다."""
+        assert type in (
+            "passage",
+            "query",
+        ), "type should be either 'passage' or 'query'"
+        if type == "passage":
+            last_hidden_states = self.passage_encoder(
+                input_ids=x, attention_mask=attn_mask
+            ).last_hidden_state
+            average_pooling = torch.mean(last_hidden_states, dim=1)
+            return average_pooling
+        else:
+            last_hidden_states = self.query_encoder(
+                input_ids=x, attention_mask=attn_mask
+            ).last_hidden_state
+            average_pooling = torch.mean(last_hidden_states, dim=1)
+            return average_pooling
+
+    def checkpoint(self, model_ckpt_path):
+        torch.save(deepcopy(self.state_dict()), model_ckpt_path)
+        print(f"model self.state_dict saved to {model_ckpt_path}")
+
+    def load(self, model_ckpt_path):
+        with open(model_ckpt_path, "rb") as f:
+            state_dict = torch.load(f)
+        self.load_state_dict(state_dict)
+        print(f"model self.state_dict loaded from {model_ckpt_path}")
